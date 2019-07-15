e4e9968a05131262cb8e9c0f0d4d6d21842c9878
gazouly <m.algazouly96@gmail.com>
2019-07-15 15:56:09 +0200

    Adding SSL to ML

--- core/httpClient.js
+++ core/httpClient.js
@@ -10,6 +10,6 @@ export const serverHttp = axios.create({
 })
 
 export const mlHttp = axios.create({
-  // baseURL: 'http://195.246.57.111:5557'
-  baseURL: 'http://172.16.220.220:5557'
+  baseURL: 'https://195.246.57.111:5557'
+  // baseURL: 'http://172.16.220.220:5557'
 })
